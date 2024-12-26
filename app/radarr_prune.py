ec1e0b282b6500b72688f8de346f853a4e09d294
Marco <marco@mjanssen.nl>
2024-12-26 13:54:38 +0100

    show percetage each run

--- app/radarr_prune.py
+++ app/radarr_prune.py
@@ -504,7 +504,7 @@ class RLP():
         isFull, percentage = self.isDiskFull()
 
         logging.info(f"Percentage diskspace radarr: {percentage}%")
-        
+
         if media and isFull:
             media.sort(key=self.sortOnTitle)  # Sort the list on Title
             for movie in media:
